df6002781110180f8528c8687421252f0c71136e
Jake Strong <strong7@pdx.edu>
2023-03-23 20:45:34 -0700

    Javascript added to the contact form

---
+++ src/project.js
@@ -0,0 +1,15 @@
+function check_submit() {
+    let fn = document.forms["form"]["firstname"].value;
+    let ln = document.forms["form"]["lastname"].value;
+    let em = document.forms["form"]["email"].value;
+    if (fn == "" && ln == "" && em == "")
+    {
+        alert("At least one field must be filled out");
+        return false;
+    }
+
+    else {
+        alert("Contact form information submitted")
+        return true;
+    }
+}
