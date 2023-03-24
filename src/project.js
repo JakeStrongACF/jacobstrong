327a9390303e132483ae1c525b1ec0eaa3c16aa9
Jake Strong <strong7@pdx.edu>
2023-03-23 22:31:13 -0700

    Added descriptions to projects and formated files.

--- src/project.js
+++ src/project.js
@@ -1,15 +1,12 @@
 function check_submit() {
-    let fn = document.forms["form"]["firstname"].value;
-    let ln = document.forms["form"]["lastname"].value;
-    let em = document.forms["form"]["email"].value;
-    if (fn == "" && ln == "" && em == "")
-    {
-        alert("At least one field must be filled out");
-        return false;
-    }
-
-    else {
-        alert("Contact form information submitted")
-        return true;
-    }
+  let fn = document.forms["form"]["firstname"].value;
+  let ln = document.forms["form"]["lastname"].value;
+  let em = document.forms["form"]["email"].value;
+  if (fn == "" && ln == "" && em == "") {
+    alert("At least one field must be filled out");
+    return false;
+  } else {
+    alert("Contact form information submitted");
+    return true;
+  }
 }
